2c1f263b6db37dc4ea4f3613de84edf8cb64921d
dianaalmeidaa <diana.p.almeida1@gmail.com>
2023-05-30 09:15:09 +0100

    Código

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-let espLarg = 20;
-let espAltura = 20;
+let espLarg = 10;
+let espAltura = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(255);
   stroke(255);
   strokeWeight(2);
   noFill();
-  background(0);
+  background(255);
 }
 
 function draw() {
@@ -15,9 +15,25 @@ function draw() {
   for (let i = 1; i < espLarg; i++) {
     for (let j = 1; j < espAltura; j++) {
       push();
-      translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
+      translate(i * width / espLarg, j * height / espAltura)
+      translate(-55,-45)
+      scale(0.08);
+      noStroke();
+   
+      
+     
+ 
+
+        noFill();
+        stroke(50,255);
+        circle(width /2, height /2, height / 4,);
+        circle(width /2 - height /5/4, height /2 - height/18, height /12 );
+        circle(width /2 + height /5/4, height /2 - height/18, height /12 );
+        ellipse(645,475,280,242);
+        ellipse(850,410,170,52);
+        ellipse(441,410,170,52);
       pop();
     }
   }
 }
+
